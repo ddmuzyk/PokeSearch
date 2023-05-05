548fac9e0ef93012d23b626d83b58f44db3cbd15
ddmuzyk <domino2k1@gmail.com>
2023-05-06 00:17:38 +0200

    base stats, calculating barchart

--- src/components/PokeCard/subPokeCard/BaseStats/BaseStats.jsx
+++ src/components/PokeCard/subPokeCard/BaseStats/BaseStats.jsx
@@ -8,10 +8,15 @@ const BaseStats = ({pokemonData}) => {
       <table>
         <tbody>
           {pokemonData.stats.map((item) => {
+            const pokeName = item.stat.name;
+            const baseStat = item.base_stat;
             return (
-            <tr className="base-stat" key={item.stat.name}>
-              <th className="base-stat-item" >{item.stat.name.toUpperCase()} : {item.base_stat}</th>
-              <th className="barchart"></th>
+            <tr className="base-stat" key={pokeName}>
+              <th className="base-stat-item" >{pokeName.toUpperCase()} : {baseStat}</th>
+              <td className="barchart-container">
+                {/* 255 is the highest possible base stat */}
+                <div className="barchart" id={`${pokeName}`} style={{width: `${baseStat/255 * 100}%`}}></div>
+              </td>
             </tr>
 
             )
